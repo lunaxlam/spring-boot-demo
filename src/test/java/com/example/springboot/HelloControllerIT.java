2720ff9fc205c4203bd527f553ec3cd0cfd302c0
Luna Lam <lunalam@Lunas-MacBook-Pro.local>
2023-10-22 21:26:47 -0700

    Added integration test

---
+++ src/test/java/com/example/springboot/HelloControllerIT.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Full-stack integration test
+// The embedded server starts on a random port because of webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+// and the actual port is configured automatically in the base URL for the TestRestTemplate.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
+    }
+}
